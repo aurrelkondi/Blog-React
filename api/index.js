ba16442b74902c997f70c9962f8e45b225f54888
aurrelkondi <aurrelkondi@hotmail.com>
2023-01-17 14:08:54 +0100

    Categories added

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoriesRoute = require("./routes/categories");
 
 mongoose.set("strictQuery", false);
 
@@ -23,6 +24,7 @@ mongoose
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoriesRoute);
 
 app.listen("5000", () => {
 	console.log("Backend is running");
