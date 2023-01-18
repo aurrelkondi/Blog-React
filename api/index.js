32c9217548e31390e9a7dbea9f318617fe9dbf53
aurrelkondi <aurrelkondi@hotmail.com>
2023-01-18 12:10:12 +0100

    Categories added

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoriesRoute = require("./routes/categories");
+const multer = require("multer");
 
 mongoose.set("strictQuery", false);
 
@@ -20,6 +21,22 @@ mongoose
 	})
 	.then(console.log("Connected to MongoDB"))
 	.catch((err) => console.log(err));
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	},
+});
+
+const upload = multer({
+	storage: storage,
+});
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+	res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
