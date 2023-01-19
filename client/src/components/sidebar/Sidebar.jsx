7c7d3a94c6cb0ab5d463e86201503d2a56760122
aurrelkondi <aurrelkondi@hotmail.com>
2023-01-19 17:12:58 +0100

    using hooks in Sidebar

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
 import "./sidebar.css";
 
 export default function Sidebar() {
-	const [cat, setCat] = useState({});
+	const [cats, setCats] = useState([]);
+
+	useEffect(() => {
+		const getCats = async () => {
+			const res = await axios.get("/categories");
+			setCats(res.data);
+		};
+		getCats();
+	}, []);
 
 	return (
 		<div className="sidebar">
@@ -21,7 +31,9 @@ export default function Sidebar() {
 			<div className="sidebarItem">
 				<span className="sidebarTitle">CATEGORIES</span>
 				<ul className="sidebarList">
-					<li className="sidebarListItem">Life</li>
+					{cats.map((c) => (
+						<li className="sidebarListItem">{c.name}</li>
+					))}
 				</ul>
 			</div>
 			<div className="sidebarItem">
